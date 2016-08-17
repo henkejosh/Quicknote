4988627d8db1331edf5a522186974aa8f633db6c
Josh Henke <henke.josh@gmail.com>
2016-08-16 21:35:57 -0700

    Updated routes

--- fullstackProj/config/routes.rb
+++ fullstackProj/config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy, :new]
     resources :users, only: [:create, :update, :new, :show]
-    
+
+    resources :notebooks do
+      resources :notes
+    end
+
+    resources :tags
+
 
   end
 end
