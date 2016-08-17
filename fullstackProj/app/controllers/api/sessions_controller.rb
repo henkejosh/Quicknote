abf67a4217344b19cd988d8d46e46bcf3af41a66
Josh Henke <henke.josh@gmail.com>
2016-08-16 21:49:13 -0700

    Began work on Users controller

--- fullstackProj/app/controllers/api/sessions_controller.rb
+++ fullstackProj/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,41 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render(
+        json: {
+          base: ["Invalid email/password combination"]
+        },
+        status: 401
+      )
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render(
+        json: {
+          base: ["Session deleted"]
+        }
+      )
+    else
+      render(
+        json: {
+          base: ["Nobody signed in"]
+        },
+        status: 404
+      )
+    end
+  end
+
+
 end
--- fullstackProj/app/models/user.rb
+++ fullstackProj/app/models/user.rb
@@ -8,18 +8,18 @@ class User < ApplicationRecord
 	after_initialize :ensure_session_token
 	before_validation :ensure_session_token_uniqueness
 
-	def password= password
+	def password=(password)
 		self.password_digest = BCrypt::Password.create(password)
 		@password = password
 	end
 
-	def self.find_by_credentials email, password
+	def self.find_by_credentials(email, password)
 		user = User.find_by(email: email)
 		return nil unless user
 		user.password_is?(password) ? user : nil
 	end
 
-	def password_is? password
+	def password_is?(password)
 		BCrypt::Password.new(self.password_digest).is_password?(password)
 	end
 
