90182c5816de87e745641ca9d80b832394eee684
Josh Henke <henke.josh@gmail.com>
2016-08-16 21:30:07 -0700

    Added StaticPages Controller. Can now render Root

---
+++ fullstackProj/app/controllers/api/users_controller.rb
@@ -0,0 +1,42 @@
+class Api::UsersController < ApplicationController
+  def new
+		@user = User.new
+		render json: @user
+	end
+
+	def show
+		@user = User.find(params[:id])
+		# render json: @user
+		# render "api/users/show"
+    render :show
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			login(@user)
+			# render "api/users/show"
+      render :show
+		else
+			render json: @user.errors, status: 422
+		end
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update_attributes!(user_params)
+		if @user.save
+			# render "api/users/show"
+      render :show
+		else
+			render json: @user.errors, status: 422
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :password)
+	end
+
+end
--- fullstackProj/config/routes.rb
+++ fullstackProj/config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy, :new]
-
+    resources :users, only: [:create, :update, :new, :show]
+    
 
   end
 end
