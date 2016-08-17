b95370ff9807cbf97fc700b2900f440267e8385a
Josh Henke <henke.josh@gmail.com>
2016-08-16 20:46:31 -0700

    Setup Rails proj., webpack, and NPM

---
+++ fullstackProj/webpack.config.js
@@ -0,0 +1,32 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/quicknote.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
