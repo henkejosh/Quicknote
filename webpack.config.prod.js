2397db7a88b310308bf45688005c74f2734d9131
Josh Henke <henke.josh@gmail.com>
2016-09-14 00:36:54 -0700

    cleaned up a few branches. Updaed webpack.config to remove React minification warning

---
+++ webpack.config.prod.js
@@ -0,0 +1,46 @@
+var path = require("path");
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/quicknote.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    noParse: /node_modules\/quill\/dist\/quill\.js/,
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins:[
+  new webpack.DefinePlugin({
+    'process.env':{
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress:{
+      warnings: true
+    }
+  })
+],
+};
